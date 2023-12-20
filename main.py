84ada94a1dba1466eb9fe6b855bba2a5f645bffa
QATON94 <V_A_N-1_94@mail.ru>
2023-12-20 12:49:48 +0300

    task_2

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from src.setings import PRODUCTS_PATH
-from src.moduls import get_category
+from src.moduls import get_category, get_product
 from src.utils import get_json_file
 
 
 def main() ->None:
-    products = get_json_file(PRODUCTS_PATH)
-    category, product = get_category(products)
-    print()
-    # print(product.price)
+    products_json = get_json_file(PRODUCTS_PATH)
+    categories = get_category(products_json)
+    products = get_product(products_json)
+    for category in categories:
+        print(category.name)
+        print(category.description)
+        print(category.commodities)
+        print(category.number_categories)
+        print(category.number_unique_products)
+        print('____________')
+
+    for product in products:
+        print(product.name)
+        print(product.description)
+        print(product.price)
+        print(product.quantity_stock)
+        print('____________')
+
 
 if __name__ == '__main__':
     main()
--- src/moduls.py
+++ src/moduls.py
@@ -5,18 +5,12 @@ class Category:
     number_categories: int
     number_unique_products: int
 
-    def __init__(self, name, description, commodities):
+    def __init__(self, name, description, commodities, number_categories):
         self.name = name
         self.description = description
         self.commodities = commodities
-        # self.number_categories = number_categories
-        # self.number_unique_products = number_unique_products
-
-    @classmethod
-    def get_quantity(cls, number_categories: int, number_unique_products: int):
-        number_categories = number_categories
-        number_unique_products = number_unique_products
-        return
+        self.number_categories = number_categories
+        self.number_unique_products = len(commodities)
 
 
 class Product:
@@ -34,27 +28,31 @@ class Product:
 
 def get_category(products: list):
     initialization_categories = []
-    initialization_products = []
-    i = '1'
     for product_category in products:
         if product_category:
             initialization_category: Category = Category(
                 name=product_category['name'],
                 description=product_category['description'],
-                commodities=get_commodities(product_category)
-            )
-            for product in product_category['products']:
-                if product:
-                    initialization_product: Product = Product(
-                        name=product['name'],
-                        description=product['description'],
-                        price=product['price'],
-                        quantity_stock=product['quantity']
-                    )
-                    initialization_products.append(initialization_product)
-            initialization_categories.append(initialization_category)
+                commodities=get_commodities(product_category),
+                number_categories=len(products))
 
-    return initialization_categories, initialization_products
+            initialization_categories.append(initialization_category)
+    return initialization_categories
+
+
+def get_product(products: list):
+    initialization_products = []
+    for product_category in products:
+        if product_category:
+            for product in product_category['products']:
+                initialization_product: Product = Product(
+                    name=product['name'],
+                    description=product['description'],
+                    price=product['price'],
+                    quantity_stock=product['quantity'])
+                initialization_products.append(initialization_product)
+
+    return initialization_products
 
 
 def get_commodities(product: dict) -> list:
@@ -63,7 +61,3 @@ def get_commodities(product: dict) -> list:
         list_products.append(name['name'])
     return list_products
 
-
-def get_number_categories(product_category: dict):
-    print(len(product_category['products']))
-    return len(product_category['products'])
