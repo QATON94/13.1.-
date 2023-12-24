7c02bc389e40aedf7ee8dd33d77837aaed9ac129
V_A_N <V_A_N-1_94@mail.ru>
2023-12-24 20:14:41 +0300

    Changing classes in moduls.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from src.moduls import get_category, get_product
+from src.moduls import Category, Product
 from src.setings import PRODUCTS_PATH
 from src.utils import get_json_file
 
 
 def main() -> None:
-    products_json = get_json_file(PRODUCTS_PATH)
-    categories = get_category(products_json)
-    products = get_product(products_json)
+    categories = Category.init_from_file(PRODUCTS_PATH)
+    products = Product.get_products(PRODUCTS_PATH)
+
     for category in categories:
         print(category.name)
         print(category.description)
-        print(category.commodities)
+        print(category.product)
         print(category.number_categories)
-        print(category.number_unique_products)
+        print(category.total_number_of_unique_products)
         print('____________')
 
     for product in products:
         print(product.name)
         print(product.description)
         print(product.price)
-        print(product.quantity_stock)
+        print(product.quantity)
         print('____________')
 
 
--- src/moduls.py
+++ src/moduls.py
@@ -1,66 +1,69 @@
-class Category:
+import json
+from typing import Any
 
-    def __init__(self, name: str, description: str, commodities: list, number_categories: int):
+
+class FromFile:
+    @staticmethod
+    def get_json_file(path: Any) -> list[dict]:
+        """
+        Получем данные с json файла
+        :param path: Путь к файлу
+        :return: Список с данными json файла
+        """
+        with open(path, encoding='utf=8') as json_file:
+            product_json = json.load(json_file)
+        return product_json
+
+
+class Category(FromFile):
+    number_categories = 0
+
+    def __init__(self, name: str, description: str, products):
         self.name = name
         self.description = description
-        self.commodities = commodities
-        self.number_categories = number_categories
-        self.number_unique_products = len(commodities)
+        self.__product = Category.get_commodities(products)
+        Category.number_categories += 1
+        self.total_number_of_unique_products = len(self.__product)
 
 
-class Product:
+    @classmethod
+    def init_from_file(cls, filename):
+        data = cls.get_json_file(filename)
+        initialization_categories = []
+        for item in data:
+            initialization_categories.append(cls(**item))
+        return initialization_categories
 
-    def __init__(self, name: str, description: str, price: float, quantity_stock: int):
+    @staticmethod
+    def get_commodities(product: dict) -> list:
+        """
+        Получаем список товаров
+        """
+        list_products = []
+        for name in product:
+            list_products.append(name['name'])
+        return list_products
+
+
+class Product(FromFile):
+
+    def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
         self.price = price
-        self.quantity_stock = quantity_stock
+        self.quantity = quantity
 
+    @classmethod
+    def get_products(cls, filename):
+        """
+        Функция инициализирует класс Product
+        :param products: Список продукций
+        :return: Список класса Product
+        """
+        data = cls.get_json_file(filename)
+        initialization_products = []
+        for item in data:
+            for prod in item['products']:
+                initialization_products.append(cls(**prod))
 
-def get_category(products: list[dict]) -> list[Category]:
-    """
-    Функция инициализирует класс Category
-    :param products: Список продукций
-    :return: Список класса Category
-    """
-    initialization_categories = []
-    for product_category in products:
-        if product_category:
-            initialization_category: Category = Category(
-                name=product_category['name'],
-                description=product_category['description'],
-                commodities=get_commodities(product_category),
-                number_categories=len(products))
-
-            initialization_categories.append(initialization_category)
-    return initialization_categories
-
-
-def get_product(products: list[dict]) -> list[Product]:
-    """
-    Функция инициализирует класс Product
-    :param products: Список продукций
-    :return: Список класса Product
-    """
-    initialization_products = []
-    for product_category in products:
-        if product_category:
-            for product in product_category['products']:
-                initialization_product: Product = Product(
-                    name=product['name'],
-                    description=product['description'],
-                    price=product['price'],
-                    quantity_stock=product['quantity'])
-                initialization_products.append(initialization_product)
-
-    return initialization_products
-
-
-def get_commodities(product: dict) -> list:
-    """
-    Получаем список товаров
-    """
-    list_products = []
-    for name in product['products']:
-        list_products.append(name['name'])
-    return list_products
+        return initialization_products
